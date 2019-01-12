d7d8f720894905db3ed629f5ccf0657f154623be
li223 <li223@users.noreply.github.com>
2019-01-12 10:18:39 +0000

    No Code No Life

--- DH.Net/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DH.Net
-{
-    public class Class1
-    {
-    }
-}
---
+++ DH.Net/UserExpLevel.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace DH.Net
+{
+    /// <summary>
+    /// User exp info object
+    /// </summary>
+    public struct UserExpLevel
+    {
+        /// <summary>
+        /// User Level
+        /// </summary>
+        [JsonProperty("lvl")]
+        public int Level { get; private set; }
+
+        /// <summary>
+        /// Total Exp of the user
+        /// </summary>
+        [JsonProperty("total_exp")]
+        public int TotalExp { get; private set; }
+
+        /// <summary>
+        /// The percentage of level completion
+        /// </summary>
+        [JsonProperty("exp_percent")]
+        public int ExpPercent { get; private set; }
+    }
+}
